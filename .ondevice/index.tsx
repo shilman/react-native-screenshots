47844ae8be942956348992234f62ccc451982ed8
Michael Shilman <shilman@users.noreply.github.com>
2024-08-18 18:19:24 +0800

    Fix snapshotting & clean up

--- .ondevice/index.tsx
+++ .ondevice/index.tsx
@@ -1,13 +1,26 @@
+import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { view } from './storybook.requires';
+import { SafeAreaView } from 'react-native';
+
+let onDeviceUI = true;
+let Storybook = () => <StorybookUIRoot />;
+if (process.env.EXPO_PUBLIC_STORYBOOK_SNAPSHOT) {
+  Storybook = () => (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StorybookUIRoot />
+    </SafeAreaView>
+  );
+  onDeviceUI = false;
+}
 
 const StorybookUIRoot = view.getStorybookUI({
   enableWebsockets: true,
-  onDeviceUI: true,
+  onDeviceUI,
   storage: {
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
   },
 });
 
-export default StorybookUIRoot;
+export default Storybook;
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,21 @@
 /** @type{import("@storybook/react-webpack5").StorybookConfig} */
 module.exports = {
-  stories: [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-react-native-web",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-react-native-web',
+    '@storybook/addon-react-native-server',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
     autodocs: true,
+  },
+  reactNativeServerOptions: {
+    host: 'localhost',
+    port: 7007,
   },
 };
