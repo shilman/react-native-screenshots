85d5ab77c13d83980dd685048409c8fc12dbfe4a
ethriel3695 <ethriel3695@gmail.com>
2024-08-29 16:33:42 -0600

    Clean up Image component

--- components/dynamic/Image.tsx
+++ components/dynamic/Image.tsx
@@ -1,48 +1,9 @@
 import React from 'react';
 
-interface ImageProps {
-  src: string;
-  ignoreRegions?: Array<{
-    top: number;
-    width: number;
-    height: number;
-    left?: number;
-  }>;
-}
-
 export const Image = ({ src }) => {
-  // const image = require(`../../${filePath}`);
   return (
     <div>
-      {/* {ignoreRegions.map((region) => (
-        <div
-          key={region.top}
-          style={{
-            background: 'black',
-            width: region.width,
-            height: region.height,
-            position: 'absolute',
-            top: region.top,
-            left: region.left || 0,
-            zIndex: 1000,
-          }}
-        ></div>
-      ))} */}
       <img src={src} alt={src} />
     </div>
   );
 };
-
-// JSON object
-// Key is the name of the file
-// Value is coordinates for ignore regions
-// Our script would look for the ignore regions and add absolute coordinates to the image with the divs we've defined
-// export const ignoreRegions = {
-//   'Image.tsx': [
-//     {
-//       x: 0,
-//       y: 0,
-//       width: 240,
-//     },
-//   ],
-// };
