179bc3cc451c6688fc09a9646d7af7d303cf6f5b
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-10-20 22:41:10 +0100

    fix: local script

--- snapshot.sh
+++ snapshot.sh
@@ -3,17 +3,15 @@
 # use this script to test locally all the steps to run the snapshot ci
 
 # remove any previous build
-rm build-*.tar.gz || true
+rm -rf build-*.tar.gz AwesomeStorybook.app
 # build the app
 eas build --profile screenshot --platform ios --local
-# rename the build to the correct name
-mv build-*.tar.gz AwesomeStorybook.tar.gz
+# unzip the app
+tar -xvzf build-*.tar.gz
 # launch the simulator
 xcrun simctl boot "iPhone 15"
 # check if the simulator is booted
 xcrun simctl bootstatus booted
-# extract the app
-tar -xvzf AwesomeStorybook.tar.gz
 # check if the simulator is booted
 xcrun simctl bootstatus booted
 # terminate the app if it is running
